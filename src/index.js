96ec97f768dbee4ccc2a7fc10dc7de75cdaaff31
asychyov <a.c4off@gmail.com>
2018-06-15 11:42:28 +0300

    allow to load 'rtl-css'-chunks instead of original

--- src/index.js
+++ src/index.js
@@ -9,10 +9,24 @@ import cssnano from 'cssnano'
 const WebpackRTLPlugin = function(options = {filename: false, options: {}, plugins: []}) {
   this.options = options
   this.chunkHashs = {};
+  this.pliginName = 'webpack-rtl-plugin';
 }
 
 WebpackRTLPlugin.prototype.apply = function(compiler) {
-  compiler.plugin('emit', (compilation, callback) => {
+  if (this.options.options.updateRuntimeChunk) {
+    const rtlFlag = this.options.rtlFlag || 'IS_RTL';
+    compiler.hooks.thisCompilation.tap(this.pliginName, compilation => {
+      compilation.mainTemplate.hooks.requireEnsure.tap(this.pliginName, (source, chunk, hash) => {
+        // already updated
+        if (source.indexOf('.rtl.css') !== -1){
+          return source;
+        }
+        return source.replace(/(var href.*)("\.css";)/i, '$1 (' + rtlFlag + ' ? ".rtl.css" : ".css");');
+      });
+    });
+  }
+
+  compiler.hooks.emit.tap(this.pliginName, (compilation) => {
     const changedChunks = compilation.chunks.filter((chunk) => {
       const name = chunk.name || chunk.id;
       const prevHash = this.chunkHashs[name];
@@ -79,7 +93,7 @@ WebpackRTLPlugin.prototype.apply = function(compiler) {
         chunk.files.push.apply(chunk.files, rtlFiles)
         cb()
       })
-    }, callback)
+    })
   })
 }
 
